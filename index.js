8e0819431f742d3a5a801dac6f8fbcec2238a9f9
Erik Martinez <martinerik911@gmail.com>
2025-01-06 13:01:27 -0500

    added basic express server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+const express = require("express");
+const app = express();
+
+// Environment variable for the port
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
