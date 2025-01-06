c0132e780fb53de3b8cf230ca43025d09bcd20f2
Erik Martinez <martinerik911@gmail.com>
2025-01-06 13:34:13 -0500

    just incase

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // Environment variable for the port
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
